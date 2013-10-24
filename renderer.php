tonyjbutler/moodle

MDL-42261 My Home: Added link to show all courses on my home page
If not forced by admin, user can see all courses by clicking on link

blocks/course_overview/renderer.php
@@ -215,7 +215,11 @@ class block_course_overview_renderer extends plugin_renderer_base {
         }
         $output = $this->output->box_start('notice');
         $plural = $total > 1 ? 'plural' : '';
-        $output .= get_string('hiddencoursecount'.$plural, 'block_course_overview', $total);
+        $a = new stdClass();
+        $a->coursecount = $total;
+        $a->showalllink = html_writer::link(new moodle_url('/my/index.php', array('mynumber' => block_course_overview::SHOW_ALL_COURSES)),
+                get_string('showallcourses'));
+        $output .= get_string('hiddencoursecount'.$plural, 'block_course_overview', $a);
         $output .= $this->output->box_end();
         return $output;
     }

catalyst/moodle-block_course_overview

renderer.php
@@ -215,7 +215,18 @@ class block_course_overview_renderer extends plugin_renderer_base {
         }
         $output = $this->output->box_start('notice');
         $plural = $total > 1 ? 'plural' : '';
-        $output .= get_string('hiddencoursecount'.$plural, 'block_course_overview', $total);
+        $config = get_config('block_course_overview');
+        // Show view all course link to user if forcedefaultmaxcourses is not empty.
+        if (!empty($config->forcedefaultmaxcourses)) {
+            $output .= get_string('hiddencoursecountwithshowall'.$plural, 'block_course_overview', $total);
+        } else {
+            $a = new stdClass();
+            $a->coursecount = $total;
+            $a->showalllink = html_writer::link(new moodle_url('/my/index.php', array('mynumber' => block_course_overview::SHOW_ALL_COURSES)),
+                    get_string('showallcourses'));
+            $output .= get_string('hiddencoursecount'.$withshowall.$plural, 'block_course_overview', $a);
+        }
+
         $output .= $this->output->box_end();
         return $output;
     }