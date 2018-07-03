f7cf20d54be39b8c7f1c108a414fc78671fa57de
Howard Miller <howardsmiller@gmail.com>
2018-07-03 13:53:07 +0100

    New instances now set themselves up in the content region rather than a side-bar. Fixes #18

--- block_course_overview.php
+++ block_course_overview.php
@@ -154,4 +154,20 @@ class block_course_overview extends block_base {
     public function hide_header() {
         return false;
     }
+
+    /**
+     * Do any additional initialization you may need at the time a new block instance is created
+     * @return boolean
+     */
+    function instance_create() {
+        global $DB;
+
+        // Bodge? Modify our own instance to make the default region the
+        // content area, not the side bar.
+        $instance = $this->instance;
+        $instance->defaultregion = 'content';
+        $DB->update_record('block_instances', $instance);
+
+        return true;
+    }
 }
